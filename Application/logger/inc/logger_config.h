2d5296df9d8dad3ff6da109f07cfc8da4a3361c5
ashaker-2 <70757329+ashaker-2@users.noreply.github.com>
2025-07-30 17:20:04 +0300

    Update and rename Logger_cfg.h to logger_config.h

---
+++ Application/logger/inc/logger_config.h
@@ -0,0 +1,11 @@
+// app/inc/logger_config.h
+
+#ifndef LOGGER_CONFIG_H
+#define LOGGER_CONFIG_H
+
+#include "logger.h" // Includes LOGGER_Config_t definition
+
+// Declare the external constant for the logger configuration
+extern const LOGGER_Config_t logger_config;
+
+#endif /* LOGGER_CONFIG_H */
