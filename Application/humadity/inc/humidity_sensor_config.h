4492afd3a9cd867a65c470d846a4e7b4ec046ce8
ashaker-2 <70757329+ashaker-2@users.noreply.github.com>
2025-07-30 16:32:37 +0300

    Update and rename humadity_cfg.h to humidity_sensor_config.h

---
+++ Application/humadity/inc/humidity_sensor_config.h
@@ -0,0 +1,11 @@
+// app/inc/humidity_sensor_config.h
+#ifndef HUMIDITY_SENSOR_CONFIG_H
+#define HUMIDITY_SENSOR_CONFIG_H
+
+#include <stdint.h>
+#include "humidity_sensor.h"
+
+extern const HUMIDITY_SENSOR_Config_t humidity_sensor_configurations[];
+extern const uint32_t HUMIDITY_NUM_SENSOR_CONFIGURATIONS;
+
+#endif /* HUMIDITY_SENSOR_CONFIG_H */
